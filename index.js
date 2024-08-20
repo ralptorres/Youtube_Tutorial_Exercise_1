f73ca5cfe558e956c9dc5d611a0a855338bf1903
ralptorres <ralp.torres@gmail.com>
2024-08-20 21:04:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+let girl = "Alenere";
+let boy = "David";
+let pronoun = "she";
+let drink = "coffee";
+
+console.log(
+  girl +
+    ", the friendly neighbor, waved at " +
+    boy +
+    " as " +
+    pronoun +
+    " walked by " +
+    boy +
+    "'s house. " +
+    boy +
+    " smiled back and invited " +
+    girl +
+    " in for a cup of " +
+    drink +
+    "."
+);
+
+alert(
+  girl +
+    ", the friendly neighbor, waved at " +
+    boy +
+    " as " +
+    pronoun +
+    " walked by " +
+    boy +
+    "'s house. " +
+    boy +
+    " smiled back and invited " +
+    girl +
+    " in for a cup of " +
+    drink +
+    "."
+);
+
+document.getElementById("katawan").innerHTML =
+  girl +
+  ", the friendly neighbor, waved at " +
+  boy +
+  " as " +
+  pronoun +
+  " walked by " +
+  boy +
+  "'s house. " +
+  boy +
+  " smiled back and invited " +
+  girl +
+  " in for a cup of " +
+  drink +
+  ".";
